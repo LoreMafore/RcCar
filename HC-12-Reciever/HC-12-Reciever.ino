41f21b3a65d35ad3082707570ed0d3bd9d18600a
Conrad Mercer <momercers@gmail.com>
2024-11-21 19:40:13 -0600

    strcmp is not working at the moment

--- HC-12-Reciever/HC-12-Reciever.ino
+++ HC-12-Reciever/HC-12-Reciever.ino
@@ -1,20 +1,46 @@
+#include <SoftwareSerial.h>
+#include <string.h>
+
+
+
+SoftwareSerial HC12(2,3); // HC-12 TX-Pins, HC-12 RX Pins
+
+int led1 = 6;
+int result;
+char stringMatch[12] = "Hello World";
+
 void setup()
 {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
+    HC12.begin(9600);
+    pinMode(led1, OUTPUT);
 }
 
 void loop() 
 {
-  if(Serial.available()>0)
-  {
-    String input = Serial.readString();
-    if(input == "Hello World")
+    // Check if HC-12 has received data
+    if (HC12.available()) 
     {
-      digitalWrite(LED_BUILTIN,HIGH);
-      delay(500);
-      digitalWrite(LED_BUILTIN, LOW); 
-    }  
-  }
+        // Read and display data on Serial Monitor
+        while (HC12.available()) 
+        {
+            char c = HC12.read();
 
+            result = strcmp(c, stringMatch);
+
+            if(result == 0)
+            {
+              Serial.write(c);
+            }
+            else
+            {
+              Serial.write(result);
+            }
+        }
+
+        // Blink LED when data is received
+        digitalWrite(led1, HIGH);
+        delay(500);
+        digitalWrite(led1, LOW);
+    }
 }
--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,15 +1,16 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial HC12(2,3); //HC-12 TX-Pins, HC-12 RX Pins
+
 void setup()
 {
-  //This starts the serial communication and set the rate of sending
-  //information to 9600 bits per sec
   Serial.begin(9600);
+  HC12.begin(9600);
 }
 
 void loop() 
-{
-  //This send the a message using serial. 
-  Serial.print("Hello World");
-  //Does not send another one for two seconds
+{ 
+  HC12.println("Hello World");
   delay(2000);
 
 }
