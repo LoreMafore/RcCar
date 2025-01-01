aeb301f71df536442662c1d8252a71676a0b3f57
Conrad Mercer <144570519+LoreMafore@users.noreply.github.com>
2024-12-31 19:14:59 -0600

    Add files via upload

--- HC-12-Reciever/HC-12-Reciever.ino
+++ HC-12-Reciever/HC-12-Reciever.ino
@@ -6,10 +6,16 @@
 SoftwareSerial HC12(7,8); // HC-12 TX-Pins, HC-12 RX Pins
 
 int led1 = 2;
+
 // Motor 1 Pins
 int ena = 3;
 int in1 = 5;
 int in2 = 6;
+
+//Motor 2 Pins
+int enb = 11;
+int in3 = 9;
+int in4 = 10;
 
 void setup()
 {
@@ -19,6 +25,9 @@ void setup()
     pinMode(ena, OUTPUT);
     pinMode(in1, OUTPUT);
     pinMode(in2, OUTPUT);
+    pinMode(enb, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
 }
 
 void loop() 
@@ -33,34 +42,51 @@ void loop()
 
             if(c == 0x31)
             {
-              Serial.write(c);
-              Serial.write("\n");
+              //Serial.println("Forward");
+              //Serial.write(c);
+              //Serial.write("\n");
               digitalWrite(led1, HIGH);
-              delay(500);
-              digitalWrite(led1, LOW);
-              
-              // Clockwise Max Spd
               digitalWrite(in1, HIGH);
               digitalWrite(in2, LOW);
-              analogWrite(ena, 50);
-              delay(2000);
+              analogWrite(ena, 75);
+              digitalWrite(in3, HIGH);
+              digitalWrite(in4, LOW);
+              analogWrite(enb, 70);
             
+            }
+
+            else if(c == 0x30)
+            {
               // Stop
+              //Serial.println("Stop");
+              //Serial.write(c);
+              //Serial.write("\n");
+              digitalWrite(led1, LOW);
               digitalWrite(in1, LOW);
               digitalWrite(in2, LOW);
               analogWrite(ena, 0);
-              delay(2000);
-            
+              digitalWrite(in3, LOW);
+              digitalWrite(in4, LOW);
+              analogWrite(enb, 0);
+            }
+            else if(c == 0x32)
+            {
               // CounterClockwise Max Spd
+              //Serial.println("Backwards");
+              //Serial.write(c);
+              //Serial.write("\n");
+              digitalWrite(led1, HIGH);
               digitalWrite(in1, LOW);
               digitalWrite(in2, HIGH);
-              analogWrite(ena, 100);
-              delay(2000);
+              analogWrite(ena, 75);
+              digitalWrite(in3, LOW);
+              digitalWrite(in4, HIGH);
+              analogWrite(enb, 70);
             }
             else
             {
-              Serial.write(c);
-              Serial.write("Not it");
+              //Serial.write(c);
+              //Serial.print("Not it");
             }
         }
     }
--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,16 +1,38 @@
 #include <SoftwareSerial.h>
 
+int y_pin = A1;
+int y_val = 0;
+char message = 0;
+
 SoftwareSerial HC12(2,3); //HC-12 TX-Pins, HC-12 RX Pins
 
 void setup()
 {
   Serial.begin(9600);
   HC12.begin(9600);
+  pinMode(y_pin, INPUT);
 }
 
 void loop() 
 { 
-  HC12.println("1");
-  delay(2000);
+  y_val = analogRead(y_pin);
+
+  if (y_val >= 0 and y_val <= 341)
+  {
+      HC12.println("1");
+      Serial.print("1");
+  }
+
+  else if (y_val >= 342 and y_val <= 682)
+  {
+    HC12.println("0");
+    Serial.print("0");
+  }
+
+  else if (y_val >= 683 and y_val <= 1023)
+  {
+    HC12.println("2");
+    Serial.print("2");
+  }
 
 }
